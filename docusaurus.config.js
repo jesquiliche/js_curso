9565225d3e4be7442d956ad3411038a455848145
Jesús Quintana <jesquiliche@hotmail.comm>
2024-07-16 19:07:00 +0200

    Herrmientas

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -19,7 +19,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'jesquiliche', // Usually your GitHub org/user name.
-  projectName: 'api_vinos_doc', // Usually your repo name.
+  projectName: 'js_curso_doc', // Usually your repo name.
   deploymentBranch:'gh-pages',
   trailingSlash:false,
 
