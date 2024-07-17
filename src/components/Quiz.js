c9fb5b01dc57bc7ec38677c7dadb9ee3bd79178a
Jesús Quintana <jesquiliche@hotmail.comm>
2024-07-18 01:21:41 +0200

    Añadiendo cuestionarios

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,123 +1,37 @@
 import React, { useState } from 'react';
 import './quiz.css';
 
-const Quiz = () => {
+const Quiz = ({ questions }) => {
   const [score, setScore] = useState(0);
   const [answered, setAnswered] = useState(false);
+  const [selectedOptions, setSelectedOptions] = useState({});
+  const [feedback, setFeedback] = useState({});
 
-  const questions = [
-    {
-      question: '¿Cuál es el operador de asignación en JavaScript?',
-      options: ['=', '==', '===', '!='],
-      answer: '='
-    },
-    {
-      question: '¿Cuál es el operador de igualdad estricta?',
-      options: ['=', '==', '===', '!=='],
-      answer: '==='
-    },
-    {
-      question: '¿Cuál es el operador de desigualdad en JavaScript?',
-      options: ['<>', '==', '!==', '!='],
-      answer: '!='
-    },
-    {
-      question: '¿Cuál es el operador de desigualdad estricta?',
-      options: ['<>', '===', '!==', '!='],
-      answer: '!=='
-    },
-    {
-      question: '¿Cuál es el operador de adición?',
-      options: ['-', '*', '/', '+'],
-      answer: '+'
-    },
-    {
-      question: '¿Cuál es el operador de sustracción?',
-      options: ['-', '*', '/', '+'],
-      answer: '-'
-    },
-    {
-      question: '¿Cuál es el operador de multiplicación?',
-      options: ['*', '+', '-', '/'],
-      answer: '*'
-    },
-    {
-      question: '¿Cuál es el operador de división?',
-      options: ['/', '*', '+', '-'],
-      answer: '/'
-    },
-    {
-      question: '¿Cuál es el operador de módulo?',
-      options: ['%', '*', '/', '+'],
-      answer: '%'
-    },
-    {
-      question: '¿Cuál es el operador de incremento en JavaScript?',
-      options: ['+', '++', '--', '**'],
-      answer: '++'
-    },
-    {
-      question: '¿Cuál es el operador de decremento en JavaScript?',
-      options: ['-', '+', '--', '++'],
-      answer: '--'
-    },
-    {
-      question: '¿Cuál es el operador de concatenación de cadenas?',
-      options: ['+', '++', '&', '&&'],
-      answer: '+'
-    },
-    {
-      question: '¿Cuál es el operador de exponenciación?',
-      options: ['**', '^', 'exp', '^^'],
-      answer: '**'
-    },
-    {
-      question: '¿Cuál es el operador lógico AND?',
-      options: ['&&', '||', '&', '|'],
-      answer: '&&'
-    },
-    {
-      question: '¿Cuál es el operador lógico OR?',
-      options: ['||', '|', '&&', 'or'],
-      answer: '||'
-    },
-    {
-      question: '¿Cuál es el operador lógico NOT?',
-      options: ['!', '!!', '~', 'not'],
-      answer: '!'
-    },
-    {
-      question: '¿Cuál es el operador ternario en JavaScript?',
-      options: ['?', ':', '?:', '??'],
-      answer: '?:'
-    },
-    {
-      question: '¿Qué operador se usa para verificar si una propiedad existe en un objeto?',
-      options: ['exists', 'in', 'has', 'contain'],
-      answer: 'in'
-    },
-    {
-      question: '¿Qué operador se usa para comparar dos valores sin convertirlos a un tipo común?',
-      options: ['=', '==', '===', '!=='],
-      answer: '==='
-    },
-    {
-      question: '¿Cuál es el operador de bit a bit AND?',
-      options: ['&&', '&', '|', '||'],
-      answer: '&'
-    }
-  ];
+  const handleOptionChange = (event, index) => {
+    const { value } = event.target;
+    setSelectedOptions((prevOptions) => ({
+      ...prevOptions,
+      [index]: value,
+    }));
+  };
 
   const handleSubmit = (event) => {
     event.preventDefault();
     let newScore = 0;
+    const newFeedback = {};
+
     questions.forEach((q, index) => {
-      const selectedOption = event.target[`question-${index}`].value;
+      const selectedOption = selectedOptions[index];
       if (selectedOption === q.answer) {
         newScore += 1;
+        newFeedback[index] = 'correcto';
+      } else {
+        newFeedback[index] = 'incorrecto';
       }
     });
+
     setScore(newScore);
+    setFeedback(newFeedback);
     setAnswered(true);
   };
 
@@ -128,15 +42,17 @@ const Quiz = () => {
         <form onSubmit={handleSubmit}>
           {questions.map((q, index) => (
             <div key={index}>
-              <h3><b>Pregunta {index+1}</b></h3>
-              <h4>{q.question}</h4>
+              <h3><b>Pregunta {index + 1}</b></h3>
+              <h4 className='option'>{q.question}</h4>
               {q.options.map((option) => (
                 <div key={option}>
                   <input
                     type="radio"
                     id={`question-${index}-${option}`}
                     name={`question-${index}`}
+                  
                     value={option}
+                    onChange={(event) => handleOptionChange(event, index)}
                     required
                   />
                   <label htmlFor={`question-${index}-${option}`}>{option}</label>
@@ -144,11 +60,24 @@ const Quiz = () => {
               ))}
             </div>
           ))}
-          <button type="submit" className='quiz-button'>Enviar</button>
+          <button type="submit" className='quiz-button mt-4'>Corregir</button>
         </form>
       ) : (
         <div>
           <p>Tu puntuación es: {score}/{questions.length}</p>
+          {questions.map((q, index) => (
+            <div key={index}>
+              <h3><b>Pregunta {index + 1}</b></h3>
+              <h4>{q.question}</h4>
+              <p>Tu respuesta: {selectedOptions[index]}</p>
+              <p className='correct'>
+                Esta respuesta es: {feedback[index]}
+              </p>
+              {feedback[index] === 'incorrecto' && (
+                <p className='incorrect'>La respuesta correcta es: {q.answer}</p>
+              )}
+            </div>
+          ))}
         </div>
       )}
     </div>
